647b4397e9eb117f70d9f26a6ae3c3d13c6d2028
Laura Málaga Brotons <laura.malaga.brotons@outlook.es>
2020-02-03 23:23:36 +0100

    version comentada

--- geometria/src/Rectangulo_LMB.java
+++ geometria/src/Rectangulo_LMB.java
@@ -1,8 +1,17 @@
-
+/**
+ * @auhtor Laura Malaga Brotons
+ * @version v1.2
+ */
 public class Rectangulo_LMB extends FiguraGeometrica_LMB {
 	private double l1;
 	private double l2;
 	
+	/**
+	 * Contructor parametrizado de la clase 
+	 * @param tipoFigura 
+	 * @param lG lado grande
+	 * @param lP lado pequeño
+	 */
 	public Rectangulo_LMB(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
