869d38877e6b529334ff2277bf451b19ba371b2a
Laura Málaga Brotons <laura.malaga.brotons@outlook.es>
2020-02-03 22:54:12 +0100

    Version Refactorizada

--- geometria/src/Circulo_LMB.java
+++ geometria/src/Circulo_LMB.java
@@ -2,12 +2,12 @@
  * @author Laura Malaga
  */
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_LMB extends FiguraGeometrica_LMB {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_LMB(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
--- geometria/src/Rectangulo_LMB.java
+++ geometria/src/Rectangulo_LMB.java
@@ -1,9 +1,9 @@
 
-public class Rectangulo extends FiguraGeometrica {
+public class Rectangulo_LMB extends FiguraGeometrica_LMB {
 	private double l1;
 	private double l2;
 	
-	public Rectangulo(String tipoFigura, double lG, double lP) {
+	public Rectangulo_LMB(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
